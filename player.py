a8b3ad64ca43bd3224f316a4e7692e6de90d2426
celestepop <celeste3popoca@gmail.com>
2022-02-18 15:11:07 -0700

    added module outlines

--- player.py
+++ player.py
@@ -1,12 +1,25 @@
+from point import Point
+from 
+
 ''' 
     File: player.py
     Author:
     Description:
 '''
 
+# Inherit projectile class?
 class Player():
     '''
         Keep track of the player's movement's and points
     '''
     def __init__():
+        pass
+
+    def score(self):
+        pass
+
+    def get_position(self):
+        pass
+
+    def set_position(self):
         pass
